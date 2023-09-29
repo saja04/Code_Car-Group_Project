390d27cb3e40dbb500aa764cd5816fb5028d824f
saja04 <saajamilf@gmail.com>
2023-09-29 13:34:29 -0300

    agregado controller y ruta de getCarsByName

--- back-end/src/Routes/index.routes.js
+++ back-end/src/Routes/index.routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const postCars = require("../Controllers/postCars");
 const getCars = require("../Controllers/getCars");
+const getCarsByName = require("../Controllers/getCarsByName");
 
 const router = Router();
 
 //ROUTES CARS
 router.post("/cars", postCars);
 router.get('/cars', getCars)
+router.get('/carsName/', getCarsByName)
 
 //ROUTES USER
 
