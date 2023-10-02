68102f847e6ccf691a4aa2d13d6fefbb85401989
manuel garcia sarza <manuelgarciasarza@gmail.com>
2023-10-02 13:34:35 -0300

    search bar

--- front-end/soyHenryFinal/src/components/card/carCard.jsx
+++ front-end/soyHenryFinal/src/components/card/carCard.jsx
@@ -3,18 +3,23 @@ import carCardStyles from "./carCard.module.css";
 import { Link } from "react-router-dom";
 
 function CarCard({ vehicle, showPricesInUSD }) {
-
   return (
     <div className={carCardStyles.conteiner}>
       <div className={carCardStyles.carCard}>
         <h2>
           {vehicle.marca} {vehicle.modelo}
         </h2>
-        <img src={vehicle.car_imagen} alt={`${vehicle.car_marca} ${vehicle.car_modelo}`} />
-        <p> {vehicle.car_marca} {vehicle.car_modelo}</p>
+        <img
+          src={vehicle.car_imagen}
+          alt={`${vehicle.car_marca} ${vehicle.car_modelo}`}
+        />
+        <p>
+          {" "}
+          {vehicle.car_marca} {vehicle.car_modelo}
+        </p>
         <p>Color: {vehicle.car_color}</p>
         <p>Año: {vehicle.car_año}</p>
-        <p>Motor: {vehicle.car_tipo_de_motor}</p>
+        <p>{vehicle.car_condicion}</p>
         <p>
           {showPricesInUSD
             ? `USD$${vehicle.car_precio_usd}`
