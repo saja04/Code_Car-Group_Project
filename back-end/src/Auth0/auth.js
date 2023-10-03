f2d6cc781ab2c0f76d9c96ec7d35a3cd685212c1
saja04 <saajamilf@gmail.com>
2023-10-03 11:28:34 -0300

    ajustes a auth0

--- back-end/src/Auth0/auth.js
+++ back-end/src/Auth0/auth.js
@@ -8,7 +8,7 @@ const config = {
   secret: SECRET,
   baseURL: 'https://codecar.onrender.com',
   clientID: 'rrRqAiM1AYESjGA1UvXV3loPDqdgY695',
-  issuerBaseURL: 'https://dev-fl1dkagbtwtxnd7y.us.auth0.com'
+  issuerBaseURL: 'http://localhost:5173/'
 };
 
 module.exports = config;
