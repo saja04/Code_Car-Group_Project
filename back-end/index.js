e0c21110e112d4db1c2570aa003dd23f0274f182
saja04 <saajamilf@gmail.com>
2023-10-04 13:54:25 -0300

    config allowedOrigins

--- back-end/index.js
+++ back-end/index.js
@@ -18,10 +18,18 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser("mi ultra secreto xd"));
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://code-car-41a-pf-enac.vercel.app, http://localhost:5173"
-  ); // CAMBIAR CUANDO SE DEPLOYEE CON EL DOMINIO AL QUE CORRESPONDA
+  const allowedOrigins = [
+    "https://code-car-41a-pf-enac.vercel.app",
+    "http://localhost:5173",
+  ];
+  
+  const origin = req.headers.origin;
+  if(allowedOrigins.includes(origin)){
+    res.header(
+      "Access-Control-Allow-Origin",
+      origin
+    );
+  }
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
