91d25c7c19eb38c60c298fddaf6b898dc4f5a8df
saja04 <saajamilf@gmail.com>
2023-10-05 18:04:38 -0300

    cambios en auth

--- back-end/src/Routes/index.routes.js
+++ back-end/src/Routes/index.routes.js
@@ -4,27 +4,33 @@ const getCarsHandler = require("../Handlers/getCarsHandler");
 const deleteCarsHandler = require("../Handlers/deleteCarsHandler");
 const getCarsByNameHandler = require("../Handlers/getCarsByNameHandler");
 const getCarsByIdHandler = require("../Handlers/getCarsByIdHandler");
-const getAllUsersHandler = require("../Handlers/getAllUsersHandler")
-const postUserHandler = require("../Handlers/postUserHandler")
-const passport = require("passport")
+const getAllUsersHandler = require("../Handlers/getAllUsersHandler");
+const postUserHandler = require("../Handlers/postUserHandler");
+const passport = require("passport");
 
 const router = Router();
+
+const checkAuthenticated = async (req, res, next) => {
+    if(req.isAuthenticated()){
+      return next()
+    }
+    res.json({msg: 'usuario no logeado, por favor log in o sign in'})
+};
 
 //ROUTES CARS
 router.post("/carsPost", postCarsHandler);
 router.post("/cars", getCarsHandler);
-router.post("/users", getAllUsersHandler)
+router.post("/users", getAllUsersHandler);
 router.get("/carsDelete/:id", deleteCarsHandler);
 router.get("/carsName/", getCarsByNameHandler);
 router.get("/cars/:id", getCarsByIdHandler);
 
-
 //ROUTES USER
 router.post("/login/succesful", (req, res) => {
-  res.json({msg: "iniciaste sesion correctamente"});
+  res.json({ msg: "iniciaste sesion correctamente" });
 });
 router.post("/login/failure", (req, res) => {
-  res.json({msg: "login fallido!"});
+  res.json({ msg: "login fallido!" });
 });
 
 router.post(
@@ -35,26 +41,18 @@ router.post(
   })
 );
 
-router.post('/logout', function(req, res, next){
-  req.logout(function(err){
-    if(err) return next(err);
-    res.status(201).json({msg: 'logout succesful'});
-  })
+router.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) return next(err);
+    return res.status(201).json({ msg: "logout succesful" });
+  });
 });
-router.post('/signup', postUserHandler)
+router.post("/signup", postUserHandler);
 
-router.get('/', async(req, res, next) => {
-  if(req.isAuthenticated()) return next();
-  res.redirect('/login/password')
-}, (req, res) => {
-  res.json({msg: 'user loged in', })
-})
-
+router.get("/", checkAuthenticated, (req, res) => {
+  res.json({msg: 'el usuairo está logeado'})
+});
 
 //ROUTES ADMIN
-
-
-
-
 
 module.exports = router;
