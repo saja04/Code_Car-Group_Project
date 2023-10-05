983115d9516cac9992d98eb8df022f47042b19a6
Zero <117301896+Mateo-Carugati@users.noreply.github.com>
2023-10-05 13:36:50 -0300

    Implementado borrado logico
    implementado la columna state, que define el estado del producto en un booleano

--- back-end/src/Models/CarModel.js
+++ back-end/src/Models/CarModel.js
@@ -50,7 +50,11 @@ module.exports = (sequelize) => {
     car_imagen: {
       type: DataTypes.STRING(20000),
       allowNull:false
-    }
+    },
+    state: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false, // Por defecto, un registro no está borrado
+    },
   },
   { timestamps: false }
   );
