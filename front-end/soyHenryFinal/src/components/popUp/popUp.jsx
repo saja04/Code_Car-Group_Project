bd314c21274460c2e3f633bb4981fd7c6c69833e
FrancoNos <fat7_2014@hotmail.com>
2023-10-12 13:40:34 -0300

    userInfo

--- front-end/soyHenryFinal/src/components/popUp/popUp.jsx
+++ front-end/soyHenryFinal/src/components/popUp/popUp.jsx
@@ -5,33 +5,35 @@ import { NavLink } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 
 function PopUp({ onClose, user }) {
-    const { logout } = useAuth0(); // Obtén logout de useAuth0
-  
-    const navigate = useNavigate();
-  
-    const handleGoToUserProfile = () => {
-      navigate('/user');
-    };
-  
-    return (
-      <div className={style.popup}>
-        <div className={style.popupContent}>
-          <div className={style.profileInfo}>
-            <p>{user.email}</p>
-            <button onClick={handleGoToUserProfile}>Mi perfil</button>
-          </div>
-          <div className={style.userActions}>
-            <h2>Mis Pedidos</h2>
-            <NavLink className={style.links} onClick={logout}>
-              Cerrar sesión
-            </NavLink>
-          </div>
+  const { user: auth0User, logout } = useAuth0(); 
+  const navigate = useNavigate();
+
+  const handleGoToUserProfile = () => {
+    const profileURL = `/user/`;
+
+    navigate(profileURL);
+  };
+
+  return (
+    <div className={style.popup}>
+      <div className={style.popupContent}>
+        <div className={style.profileInfo}>
+          <p>{user.email}</p>
+          <button onClick={handleGoToUserProfile}>Mi perfil</button>
+        </div>
+        <div className={style.userActions}>
+          <h2>Mis Pedidos</h2>
+          <NavLink className={style.links} onClick={logout}>
+            Cerrar sesión
+          </NavLink>
+         
         </div>
       </div>
-    );
-  }
-  
-  export default PopUp;
-  
+    </div>
+  );
+}
+
+export default PopUp;
+
 
 
