fc3b7ba1d0204e92bdc7dbf1dd812a706965b77c
saja04 <saajamilf@gmail.com>
2023-10-15 23:56:54 -0300

    ajustado limite de peso requests

--- back-end/index.js
+++ back-end/index.js
@@ -13,8 +13,8 @@ const server = express();
 server.name = "server";
 
 
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
+server.use(express.urlencoded({ extended: true, limit: "50mb" }));
+server.use(express.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 
@@ -36,7 +36,7 @@ server.use("/", router);
 
 
 
-conn.sync({ force: true }).then(async () => {
+conn.sync({ force: false }).then(async () => {
   console.log("db connected");
   await saveApiData();
   server.listen(3001, () => {
