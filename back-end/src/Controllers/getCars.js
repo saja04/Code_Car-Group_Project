2d95cc29a8c58a5866ffb891bb5a87acac63a3a2
saja04 <saajamilf@gmail.com>
2023-10-18 20:27:25 -0300

    agregados detalles a mis pedidos y a la dashboard de compras

--- back-end/src/Controllers/getCars.js
+++ back-end/src/Controllers/getCars.js
@@ -3,7 +3,9 @@ const sequelize = require("sequelize");
 
 const getCars = async (req) => {
   const { filter, order, pagination, precio } = req.body;
-  const query = {};
+  const query = {
+    where: { deleted: false}
+  };
 
   if (pagination) {
     query.offset = pagination.offset;
@@ -12,8 +14,11 @@ const getCars = async (req) => {
   if (order) {
     query.order = [[sequelize.literal(order.value), order.sequence]];
   }
-  if (filter) query.where = filter;
-  if (precio) query.attributes = { exclude: [precio], deleted: true };
+  if (filter) {
+    query.where = filter;
+    query.where.deleted = false
+  }
+  if (precio) query.attributes = { exclude: [precio] };
 
   return await Car.findAll(query);
 };
