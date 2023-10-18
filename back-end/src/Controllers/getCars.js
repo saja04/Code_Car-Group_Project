b25acd8031555d02ceb3881e7f9988c4e6bc7e9c
saja04 <saajamilf@gmail.com>
2023-10-18 13:41:08 -0300

    config mp

--- back-end/src/Controllers/getCars.js
+++ back-end/src/Controllers/getCars.js
@@ -3,11 +3,7 @@ const sequelize = require("sequelize");
 
 const getCars = async (req) => {
   const { filter, order, pagination, precio } = req.body;
-  const query = {
-    where: {
-      deleted: false,
-    },
-  };
+  const query = {};
 
   if (pagination) {
     query.offset = pagination.offset;
@@ -16,9 +12,7 @@ const getCars = async (req) => {
   if (order) {
     query.order = [[sequelize.literal(order.value), order.sequence]];
   }
-  console.log(filter);
-  if (filter) query.where = {...query.where, filter};
-  if (precio) query.attributes = { exclude: [precio] };
+  if (precio) query.attributes = { exclude: [precio], deleted: true };
 
   return await Car.findAll(query);
 };
