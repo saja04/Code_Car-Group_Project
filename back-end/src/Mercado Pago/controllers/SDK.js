6ae58762ab98631c82f636d061c4cf46f405f394
saja04 <saajamilf@gmail.com>
2023-10-18 01:31:24 -0300

    tercera prueba mp funcionando

--- back-end/src/Mercado Pago/controllers/SDK.js
+++ back-end/src/Mercado Pago/controllers/SDK.js
@@ -54,7 +54,7 @@ const receiveWebhook = async (req, res) => {
   try {
     if (payment.type === "payment") {
       const data = await mercadopago.payment.findById(payment["data.id"]);
-	  console.log(data);
+	  console.log(data.body.items);
 	  const searchInDb = await UserOrder.findOne({where: {car_order: data.description}})
 	  searchInDb.order_status = 'listoARetirar'
 	  await searchInDb.save()
