92b06b189861b1452ccf6ddd744c4237ff1948f7
saja04 <saajamilf@gmail.com>
2023-10-19 12:56:12 -0300

    config edit orderstatus

--- back-end/src/Routes/index.routes.js
+++ back-end/src/Routes/index.routes.js
@@ -5,29 +5,22 @@ const deleteCarsHandler = require("../Handlers/deleteCarsHandler");
 const getCarsByNameHandler = require("../Handlers/getCarsByNameHandler");
 const getCarsByIdHandler = require("../Handlers/getCarsByIdHandler");
 const buyCarHandler = require("../Handlers/buyCarHandler");
-const ReviewHandler = require("../Handlers/ReviewHandler")
+const ReviewHandler = require("../Handlers/ReviewHandler");
 
-
-const getAllOrders = require('../Controllers/compras/getAllOrders')
-const addStockHandler = require("../Handlers/addStockHandler.js")
-const deleteStockHandler = require("../Handlers/deleteStockHandler.js")
-const getUserOrderHandler = require('../Handlers/Compras/getUserOrderHandler')
-
+const getAllOrders = require("../Controllers/compras/getAllOrders");
+const addStockHandler = require("../Handlers/addStockHandler.js");
+const deleteStockHandler = require("../Handlers/deleteStockHandler.js");
+const getUserOrderHandler = require("../Handlers/Compras/getUserOrderHandler");
 
 const userCheck = require("../Authentication/userCheckController");
-const adminCheck = require('../Authentication/adminCheck')
-const moderatorCheck = require('../Authentication/moderatorCheck')
+const adminCheck = require("../Authentication/adminCheck");
+const moderatorCheck = require("../Authentication/moderatorCheck");
 const getAllUsersHandler = require("../Handlers/getAllUsersHandler");
 const getUserInfo = require("../Authentication/getUserInfo");
 const adminUser = require("../Authentication/adminUser");
-const updateUserInfo = require('../Authentication/updateUserInfo')
+const updateUserInfo = require("../Authentication/updateUserInfo");
 
-
-
-
-const allReviewsHandler = require("../Handlers/allReviewsHandler")
-
-
+const allReviewsHandler = require("../Handlers/allReviewsHandler");
 
 const router = Router();
 require("dotenv").config();
@@ -52,8 +45,8 @@ router.post("/create-order/", createOrder);
 router.post("/webhook", receiveWebhook);
 router.get("/success", (req, res) => res.send("Success!"));
 router.get("/pending", (req, res) => res.send("Pending..."));
-router.post("/review", ReviewHandler)
-router.get("/allreview", allReviewsHandler)
+router.post("/review", ReviewHandler);
+router.get("/allreview", allReviewsHandler);
 
 //ROUTES USER
 
@@ -61,23 +54,20 @@ router.get("/users", getAllUsersHandler);
 
 router.post("/userInfo", userCheck, getUserInfo);
 
-router.post('/userById', getUserById)
+router.post("/userById", getUserById);
 
 router.post("/banUser", adminUser);
 
 router.post("/updateUser/", updateUserInfo);
 
 //ROUTES ADMIN
-router.get('/carsAdmin', getCarsAdmin)
-router.post("/addStock", addStockHandler)
-router.post("/deleteStock", deleteStockHandler)
+router.get("/carsAdmin", getCarsAdmin);
+router.post("/addStock", addStockHandler);
+router.post("/deleteStock", deleteStockHandler);
 
-router.post('/allOrders', getAllOrders);
-router.post('/userOrder', getUserOrderHandler);
-router.post('/createOrderCar', createOrderHandler);
-router.post('/editOrder', changeOrderStatus)
-
-
-
+router.post("/allOrders", getAllOrders);
+router.post("/userOrder", getUserOrderHandler);
+router.post("/createOrderCar", createOrderHandler);
+router.post("/editOrder", changeOrderStatus);
 
 module.exports = router;
