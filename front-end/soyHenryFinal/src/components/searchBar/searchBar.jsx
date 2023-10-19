e627a1f16659f56f692922c127b7995fdf57fee0
FrancoNos <fat7_2014@hotmail.com>
2023-10-19 02:01:56 -0300

    search y alert

--- front-end/soyHenryFinal/src/components/searchBar/searchBar.jsx
+++ front-end/soyHenryFinal/src/components/searchBar/searchBar.jsx
@@ -2,14 +2,17 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { getCarByName } from "../../redux/actions";
 import style from "./searchBar.module.css";
+import { useNavigate } from "react-router-dom";
 
 function SearchBar({ onSearch }) {
   const divisa = localStorage.getItem('divisa');
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
+      navigate(`/tienda/`);
       dispatch(getCarByName(searchTerm, divisa));
       onSearch();
     }
