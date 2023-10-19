785fc0b87137097c674f8818fffaf064c603811f
saja04 <saajamilf@gmail.com>
2023-10-19 12:29:02 -0300

    config nodemailer

--- back-end/nodeMailer.js
+++ back-end/nodeMailer.js
@@ -5,9 +5,9 @@ const nodemailer = require('nodemailer');
 const {NODEMAILER_EMAIL, NODEMAILER_PASSWORD} = process.env
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp.office365.com",
+  port: 587,
+  secure: false,
   auth: {
     user: NODEMAILER_EMAIL,
     pass: NODEMAILER_PASSWORD,
