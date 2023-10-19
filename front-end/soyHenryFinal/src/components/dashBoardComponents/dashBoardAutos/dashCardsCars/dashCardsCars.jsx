724b56bc5d916dc0a1a0b907a4a47132b70e35a7
manuel garcia sarza <manuelgarciasarza@gmail.com>
2023-10-19 02:25:14 -0300

    a

--- front-end/soyHenryFinal/src/components/dashBoardComponents/dashBoardAutos/dashCardsCars/dashCardsCars.jsx
+++ front-end/soyHenryFinal/src/components/dashBoardComponents/dashBoardAutos/dashCardsCars/dashCardsCars.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import carCardStyles from "./dashCardscard.module.css";
+import carCardStyles from "./dashCardsCar.module.css";
 
 function DashCardsCar({ vehicle }) {
   const [isDeleted, setIsDeleted] = useState(vehicle.deleted);
